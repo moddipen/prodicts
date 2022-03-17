84260ad7ae38dce26cf211d1ab5f32041467382e
Dipen Modi <moddipen@gmail.com>
2022-03-17 14:52:03 +0530

    Login an d Register Flow

---
+++ application/models/User_model.php
@@ -0,0 +1,56 @@
+<?php 
+class User_model extends CI_Model
+{
+    function insert($data)
+    {
+        $this->db->insert('users', $data);
+        return $this->db->insert_id();
+    }
+
+    function verify_email($key){
+        $this->db->where('verification_key',$key);
+        $this->db->where('isVerified',0);
+        $query = $this->db->get('users');
+        if($query->num_rows()>0){
+            $data = array(
+                'isVerified' => 1
+            );
+            $this->db->where('verification_key', $key);
+            $this->db->update('users',$data);
+            return true;
+        } else{
+            return false;
+        }
+    }
+
+
+    function can_login($email,$password)
+    {
+        $this->db->where('email', $email);
+        $query = $this->db->get('users');
+        if($query->num_rows()>0){
+            
+            foreach ($query->result() as $row)
+            if($row->isVerified == '1')
+            {
+                $store_password = md5($password);
+                
+                if($store_password == $row->password)
+                {
+                    $this->session->set_userdata('id',$row->id);
+                } else{
+                    return "Wrong Password";
+                }
+            } else {
+                return 'Email address not verified';
+            }
+
+        } else {
+            return 'Wrong Email Address';
+        }
+    }
+
+
+}
+
+?>
